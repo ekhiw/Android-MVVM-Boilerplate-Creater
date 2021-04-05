81b76f536f7d9bd8e34a41f0d1dd9fb00ebd888b
ekhiw <ekkyhiw@gmail.com>
2021-04-05 19:47:12 +0700

    [update] change to chucknorris

--- mvvm_creater.sh
+++ mvvm_creater.sh
@@ -70,8 +70,8 @@ dependencies(){
 	echo "	implementation \"com.google.dagger:dagger:2.28.3\""
 	echo "	kapt \"com.google.dagger:dagger-compiler:2.28.3\""
 	echo "	implementation \"com.google.dagger:dagger-android:2.28.3\""
-    	echo "	implementation \"com.google.dagger:dagger-android-support:2.28.3\""
-    	echo "	kapt \"com.google.dagger:dagger-android-processor:2.28.3\""
+    echo "	implementation \"com.google.dagger:dagger-android-support:2.28.3\""
+    echo "	kapt \"com.google.dagger:dagger-android-processor:2.28.3\""
 }
 
 #########################################################
@@ -125,94 +125,174 @@ mkdir data
 cd data/
 mkdir api
 cd api/
-cat << EOF >> MainApi.kt
+cat << EOF >> ChuckApi.kt
 package $3.data.api
 
-import $3.utils.Endpoints
-import com.google.gson.JsonObject
-import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
-
-interface MainApi {
-	/**
-		@GET(Endpoints.SOME_ENDPOINT)
-		fun someTest(@Path("pathKey") mPathKey: String, @Query("someQuery") mSomeQuery: String): Call<JsonObject>
-	*/
-}
-EOF
-cat << EOF >> MainDataSource.kt
-package $3.data.api
-
-import android.app.Application
-import androidx.lifecycle.MediatorLiveData
-import $3.R
-import $3.data.model.SomeModel
-import $3.utils.ISTDateDeserializer
-import $3.utils.Resource
-import com.google.gson.JsonObject
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
+import $3.data.model.ChuckResponse
 import retrofit2.Response
-import java.util.*
-import kotlin.collections.ArrayList
+import retrofit2.http.GET
 
-class MainDataSource(
-    private val mainApi: MainApi,
-    private val application: Application
-) {
-	/**
-		private var mSomeData: MediatorLiveData<Resource<ArrayList<SomeModel>>>? = null
-	    	fun someTest(): MediatorLiveData<Resource<ArrayList<SomeModel>>> {
-		mSomeData = MediatorLiveData()
-		mSomeData?.value = Resource.loading(null)
+interface ChuckApi {
+	
+    @GET("/jokes/random")
+    suspend fun getChuck(
+    ): Response<ChuckResponse>
 
-		mainApi.someTest(mSomeQuery).enqueue(object :
-		    Callback<JsonObject> {
-		    override fun onFailure(call: Call<JsonObject>, t: Throwable) {}
-
-		    override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
-		        if (response.isSuccessful && response.body() != null) {
-		            val mJSONObject = JSONObject(response.body().toString())
-
-		            try {} catch (e: Exception) {
-		                e.printStackTrace()
-		            }
-		        } else {}
-		    }
-
-		})
-
-		return mSomeData!!
-	    	}
-	*/
 }
 EOF
-cd -
+cd ../
+cat << EOF >> AppCustomDao.kt
+package $3.data
+
+import $3.data.model.AppCustomPreset
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * The Data Access Object for the AppCustomPreset class.
+ */
+@Dao
+interface AppCustomDao {
+
+    @Query("SELECT * FROM custom_preset ORDER BY preset_name")
+    fun getAppCustomPresets(): Flow<List<AppCustomPreset>>
+
+    @Query("SELECT * FROM custom_preset WHERE id = :id")
+    fun getAppCustomPresetById(id: String): Flow<AppCustomPreset>
+
+    @Query("SELECT * FROM custom_preset WHERE preset_name = :presetName")
+    fun getAppCustomPresetByName(presetName: String): Flow<AppCustomPreset>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(plants: List<AppCustomPreset>)
+}
+EOF
+cat << EOF >> TokenDao.kt
+package $3.data
+
+import $3.data.model.UserToken
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * The Data Access Object for the Token class.
+ */
+@Dao
+interface TokenDao {
+
+    @Query("SELECT * FROM token WHERE id = :id")
+    fun getToken(id: String): Flow<UserToken>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(userToken: UserToken)
+}
+EOF
+cat << EOF >> AppDatabase.kt
+package $3.data
+
+import $3.data.model.AppCustomPreset
+import $3.data.model.UserToken
+import $3.data.worker.AppCustomPresetWorker
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+
+/**
+ * The Room database for this app
+ */
+@Database(entities = [ UserToken::class, AppCustomPreset::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun tokenDao(): TokenDao
+    abstract fun appCustomDao() : AppCustomDao
+
+    companion object {
+
+        // For Singleton instantiation
+        @Volatile private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        // Create and pre-populate the database. See this article for more details:
+        // https://medium.com/google-developers/7-pro-tips-for-room-fbadea4bfbd1#4785
+        private fun buildDatabase(context: Context): AppDatabase {
+
+            val DATABASE_NAME = "app-db"
+            return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
+                .addCallback(
+                    object : RoomDatabase.Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            val request = OneTimeWorkRequestBuilder<AppCustomPresetWorker>().build()
+                            WorkManager.getInstance(context).enqueue(request)
+                        }
+                    }
+                )
+                .build()
+        }
+    }
+}
+EOF
 mkdir model
 cd model/
-cat << EOF >> SomeModel.kt
+cat << EOF >> AppCustomPreset.kt
 package $3.data.model
 
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-data class SomeModel(
+@Entity(tableName = "custom_preset")
+data class AppCustomPreset(
+    @PrimaryKey @ColumnInfo(name = "id")  val id: String,
+    val preset_name : String,
+    val title : String,
+    val logo_big : String,
+    val logo_thumbnail : String,
+    val primary_color : String,
+    val secondary_color : String,
+    val accent_color : String,
+)
+EOF
+cat << EOF >> ChuckResponse.kt
+package $3.data.model
 
-	/**
-	@SerializedName("key1")
-	@Expose
-	val mKey1:  Int,
+data class ChuckResponse(
+    val icon_url: String,
+    val id: String,
+    val url: String,
+    val value: String,
+)
+EOF
+cat << EOF >> UserToken.kt
+package $3.data.model
 
-	@SerializedName("key2")
-	@Expose
-	val mKey2: Int,
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-	*/
-
-) {}
+@Entity(tableName = "token")
+data class UserToken(
+    @PrimaryKey @ColumnInfo(name = "id")  val id: String,
+    val jwt: String,
+    val time: String,
+)
 EOF
 cd ../
 mkdir repository
@@ -220,349 +300,105 @@ cd repository/
 cat << EOF >> MainRepository.kt
 package $3.data.repository
 
-import androidx.lifecycle.MediatorLiveData
-import $3.data.api.MainDataSource
-import $3.data.model.SomeModel
+import $3.data.model.ChuckResponse
 import $3.utils.Resource
 
+interface MainRepository {
 
-class MainRepository(private val dataSource: MainDataSource) {
-	/**
-	fun someTest(): MediatorLiveData<Resource<ArrayList<SomeModel>>> {
-		return dataSource.someTest()
-    	}
-	*/
+    suspend fun getChuck(): Resource<ChuckResponse>
+
+    suspend fun testDatabase()
+
+    suspend fun testRead()
 }
 EOF
-cd -
-cd ../
-mkdir di
-cd di/
-cat << EOF >> ActivityBuildersModule.kt
-package $3.di
+cat << EOF >> DefaultMainRepository.kt
+package $3.data.repository
 
-import $3.di.base.ViewModelModule
-import dagger.Module
-import dagger.android.ContributesAndroidInjector
-
-@Module
-abstract class ActivityBuildersModule {
-
-    @ContributesAndroidInjector(
-        modules = [
-		/** uncomment the below if you use fragments builder module (recommended) */
-		// FragmentBuildersModule::class,
-		ViewModelModule::class, MainModule::class
-        ]
-    )
-	/** uncomment and check with your own main activity */
-	// abstract fun contributeMainActivity(): MainActivity
-}
-EOF
-cat << EOF >> MainModule.kt
-package $3.di
-
-import android.app.Application
-import $3.data.api.MainApi
-import $3.data.api.MainDataSource
-import $3.data.repository.MainRepository
-import dagger.Module
-import dagger.Provides
-import retrofit2.Retrofit
-
-@Module
-class MainModule {
-
-    companion object {
-        @Provides
-        fun provideMainApi(retrofit: Retrofit): MainApi {
-            return retrofit.create(MainApi::class.java)
-        }
-
-        @Provides
-        fun provideMainDataSource(
-            mainApi: MainApi, application: Application
-        ): MainDataSource {
-            return MainDataSource(mainApi, application)
-        }
-
-        @Provides
-        fun provideMainRepository(
-            mDataSource: MainDataSource
-        ): MainRepository {
-            return MainRepository(mDataSource)
-        }
-    }
-}
-EOF
-mkdir base
-cd base/
-cat << EOF >> AppComponent.kt
-package $3.di.base
-
-import android.app.Application
-import $3.$2Application
-import $3.di.ActivityBuildersModule
-import dagger.BindsInstance
-import dagger.Component
-import dagger.android.AndroidInjector
-import dagger.android.support.AndroidSupportInjectionModule
-import javax.inject.Singleton
-
-@Singleton
-@Component(
-    modules = [AndroidSupportInjectionModule::class,
-        ActivityBuildersModule::class,
-        AppModule::class,
-        ViewModelFactoryModule::class
-    ]
-)
-
-interface AppComponent : AndroidInjector<$2Application> {
-
-    @Component.Factory
-    interface Factory {
-        fun create(@BindsInstance application: Application): AppComponent
-    }
-
-}
-EOF
-cat << EOF >> AppModule.kt
-package $3.di.base
-
-import $3.utils.Endpoints.BASE_URL
-import dagger.Module
-import dagger.Provides
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import java.util.concurrent.TimeUnit
-import javax.inject.Singleton
-
-@Module
-class AppModule {
-
-    companion object {
-
-        @Singleton
-        @Provides
-        fun provideOkHttpClient(): OkHttpClient {
-            return OkHttpClient.Builder()
-                .connectTimeout(45, TimeUnit.SECONDS)
-                .writeTimeout(45, TimeUnit.SECONDS)
-                .readTimeout(45, TimeUnit.SECONDS)
-                .build()
-        }
-
-        @Provides
-        fun provideRetrofitInstance(client: OkHttpClient): Retrofit {
-            return Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(ScalarsConverterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build()
-        }
-
-    }
-}
-EOF
-cat << EOF >> ViewModelFactoryModule.kt
-package $3.di.base
-
-import androidx.lifecycle.ViewModelProvider
-import $3.ui.base.ViewModelFactory
-import dagger.Binds
-import dagger.Module
-
-@Module
-abstract class ViewModelFactoryModule {
-
-    @Binds
-    abstract fun bindViewModelFactory(viewModelProvidersFactory: ViewModelFactory): ViewModelProvider.Factory
-}
-EOF
-cat << EOF >> ViewModelKey.kt
-package $3.di.base
-
-import androidx.lifecycle.ViewModel
-import dagger.MapKey
-import kotlin.reflect.KClass
-
-@Target(AnnotationTarget.FUNCTION,
-        AnnotationTarget.PROPERTY_GETTER,
-        AnnotationTarget.PROPERTY_SETTER)
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-@MapKey
-internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
-EOF
-cat << EOF >> ViewModelModule.kt
-package $3.di.base
-
-import androidx.lifecycle.ViewModel
-import $3.ui.main.viewmodel.MainViewModel
-import dagger.Binds
-import dagger.Module
-import dagger.multibindings.IntoMap
-
-@Module
-abstract class ViewModelModule {
-
-    @Binds
-    @IntoMap
-    @ViewModelKey(MainViewModel::class)
-    abstract fun bindViewModel(viewModel: MainViewModel): ViewModel
-}
-EOF
-cd ../../
-cat << EOF >> $2Application.kt
-package $3
-
-import $3.di.base.DaggerAppComponent
-import dagger.android.AndroidInjector
-import dagger.android.support.DaggerApplication
-
-class $2Application: DaggerApplication() {
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        return DaggerAppComponent.factory().create(this)
-    }
-}
-EOF
-mkdir utils
-cd utils/
-cat << EOF >> Endpoints.kt
-package $3.utils
-
-object Endpoints {
-    const val BASE_URL = "https://www.somesample.url/"
-
-    const val SOME_ENDPOINT = "someendpoint/{pathKey}" // ?someQuery=someValue
-
-}
-EOF
-cat << EOF >> ISTDateDeserializer.kt
-package $3.utils
-
-import com.google.gson.JsonDeserializationContext
-import com.google.gson.JsonDeserializer
-import com.google.gson.JsonElement
-import java.lang.reflect.Type
-import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.util.*
-
-class ISTDateDeserializer : JsonDeserializer<Date> {
-
-    override fun deserialize(
-        element: JsonElement?,
-        typeOfT: Type?,
-        context: JsonDeserializationContext?
-    ): Date? {
-        val date = element!!.asString
-
-        if (date.matches("^[0-9]*\$".toRegex())) {
-            return Date(date.toLong())
-        } else {
-            val format = when {
-                date.contains("Z", ignoreCase = true) -> {
-                    SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH)
-                }
-                date.contains(":") -> {
-                    SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
-                }
-                else -> {
-                    SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
-                }
-            }
-
-            format.timeZone = TimeZone.getTimeZone("IST")
-
-            return try {
-                format.parse(date)!!
-            } catch (exp: ParseException) {
-                exp.printStackTrace()
-                null
-            }
-        }
-
-    }
-}
-EOF
-cat << EOF >> Resource.kt
-package $3.utils
-
-data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
-
-    companion object {
-
-        fun <T> success(data: T?): Resource<T> {
-            return Resource(Status.SUCCESS, data, null)
-        }
-
-        fun <T> error(msg: String, data: T?): Resource<T> {
-            return Resource(Status.ERROR, data, msg)
-        }
-
-        fun <T> loading(data: T?): Resource<T> {
-            return Resource(Status.LOADING, data, null)
-        }
-
-    }
-
-}
-EOF
-cat << EOF >> Status.kt
-package $3.utils
-
-enum class Status {
-    SUCCESS,
-    ERROR,
-    LOADING
-}
-EOF
-cd ../
-mkdir ui
-cd ui/
-mkdir base
-cd base/
-cat << EOF >> ViewModelFactory.kt
-package $3.ui.base
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import javax.inject.Inject
-import javax.inject.Provider
-
-class ViewModelFactory @Inject constructor(private val viewModels: MutableMap<Class<out ViewModel>, Provider<ViewModel>>) :
-    ViewModelProvider.Factory {
-
-    override fun <T : ViewModel> create(modelClass: Class<T>): T =
-        viewModels[modelClass]?.get() as T
-}
-EOF
-cd ../
-mkdir main
-cd main/
-mkdir view
-mkdir viewmodel
-cd viewmodel/
-cat << EOF >> MainViewModel.kt
-package $3.ui.main.viewmodel
-
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.ViewModel
-import $3.data.repository.MainRepository
+import $3.data.AppCustomDao
+import $3.data.TokenDao
+import $3.api.ChuckApi
+import $3.ChuckResponse
+import $3.data.model.UserToken
 import $3.utils.Resource
+
+import com.orhanobut.logger.Logger
+import kotlinx.coroutines.flow.collect
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(
-    private val repository: MainRepository
-) : ViewModel() {}
+class DefaultMainRepository @Inject constructor(
+    private val api: ChuckApi,
+    private val tokenDao: TokenDao,
+    private val appCustomDao: AppCustomDao
+) : MainRepository {
+
+    override suspend fun getChuck(): Resource<ChuckResponse> {
+        return try {
+            val response = api.getChuck()
+            val result = response.body()
+            if(response.isSuccessful && result != null) {
+                Resource.Success(result)
+            } else {
+                Resource.Error(response.message())
+            }
+        } catch(e: Exception) {
+            Resource.Error(e.message ?: "An error occured")
+        }
+    }
+
+    override suspend fun testDatabase() {
+        tokenDao.insertAll(UserToken("1","test","test"))
+    }
+
+    override suspend fun testRead(){
+        tokenDao.getToken("1").collect {
+            Logger.i("response ${it.time}")
+        }
+    }
+
+
+}
 EOF
-cd ../
-cd -
-cd -
+mkdir worker
+cd worker/
+cat << EOF >> AppCustomPresetWorker.kt
+package $3.data.worker
+
+import $3.data.AppDatabase
+import $3.data.model.AppCustomPreset
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
+import kotlinx.coroutines.coroutineScope
+
+class AppCustomPresetWorker(
+    context: Context,
+    workerParams: WorkerParameters
+) : CoroutineWorker(context, workerParams) {
+    override suspend fun doWork(): Result = coroutineScope {
+        try {
+            applicationContext.assets.open("app_custom.json").use { inputStream ->
+                JsonReader(inputStream.reader()).use { jsonReader ->
+                    val presetType = object : TypeToken<List<AppCustomPreset>>() {}.type
+                    val presetList: List<AppCustomPreset> =
+                        Gson().fromJson(jsonReader, presetType)
+
+                    val database = AppDatabase.getInstance(applicationContext)
+                    database.appCustomDao().insertAll(presetList)
+
+                    Result.success()
+                }
+            }
+        } catch (ex: Exception) {
+            Result.failure()
+        }
+    }
+}
+EOF
 
 echo
 echo "----------DONE!----------"
