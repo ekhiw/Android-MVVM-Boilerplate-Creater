bf003428a66696739ac6577a4cdb0f545cd9d96b
ekhiw <ekkyhiw@gmail.com>
2021-04-07 10:53:03 +0700

    [update] add DI

--- mvvm_creater.sh
+++ mvvm_creater.sh
@@ -400,6 +400,133 @@ class AppCustomPresetWorker(
 }
 EOF
 
+cd $1
+mkdir di
+cd di/
+cat << EOF >> AppModule.kt
+package $3.di
+
+import $3.BuildConfig
+import $3.data.AppCustomDao
+import $3.data.TokenDao
+import $3.data.api.ChuckApi
+import $3.data.repository.DefaultMainRepository
+import $3.data.repository.MainRepository
+import $3.utils.DispatcherProvider
+
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    private const val TIMEOUT_IN_SECONDS = 15L
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(@ApplicationContext context: Context): OkHttpClient {
+        return OkHttpClient.Builder()
+            .apply {
+
+                if (BuildConfig.DEBUG_BUILD) {
+                    addInterceptor(
+                        ChuckerInterceptor.Builder(context)
+                            .collector(ChuckerCollector(context))
+                            .maxContentLength(250000L)
+                            .redactHeaders(emptySet())
+                            .alwaysReadResponseBody(true)
+                            .build()
+                    )
+                }
+                connectTimeout(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS)
+                writeTimeout(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS)
+                readTimeout(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS)
+            }
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideCurrencyApi(okHttpClient: OkHttpClient): ChuckApi = Retrofit.Builder()
+        .client(okHttpClient)
+        .baseUrl(BuildConfig.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ChuckApi::class.java)
+
+    @Singleton
+    @Provides
+    fun provideMainRepository(
+        api: ChuckApi,
+        tokenDao: TokenDao,
+        appCustomDao: AppCustomDao
+    ): MainRepository = DefaultMainRepository(api,tokenDao, appCustomDao)
+
+    @Singleton
+    @Provides
+    fun provideDispatchers(): DispatcherProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+    }
+}
+EOF
+cat << EOF >> DatabaseModule.kt
+package $3.di
+
+import $3.data.AppCustomDao
+import $3.data.AppDatabase
+import $3.data.TokenDao
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return AppDatabase.getInstance(context)
+    }
+
+    @Provides
+    fun provideTokenDao(appDatabase: AppDatabase): TokenDao {
+        return appDatabase.tokenDao()
+    }
+
+    @Provides
+    fun provideAppCustomDao(appDatabase: AppDatabase): AppCustomDao {
+        return appDatabase.appCustomDao()
+    }
+}
+EOF
+
 echo
 echo "----------DONE!----------"
 
